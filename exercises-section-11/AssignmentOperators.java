09a7b7899693913720d9659962f5454afabd8f76
carolineanjos <carolineanjoss02@gmail.com>
2025-09-11 10:21:30 -0300

    chore(java): add assignment operators study exercise

---
+++ exercises-section-11/AssignmentOperators.java
@@ -0,0 +1,12 @@
+public class AssignmentOperators {
+	public static void main(String[] args) {
+		int number = 6;
+
+		number += 3;
+		number -= 2;
+		number *= 4;
+		number /= 2;
+
+		System.out.println(number);
+	}
+}
