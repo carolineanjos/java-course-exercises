9fb99ede4c7d5e50068c805498042b76c195bcb0
carolineanjos <carolineanjoss02@gmail.com>
2025-09-13 09:44:51 -0300

    chore: add simple calculator exercise

---
+++ exercises-section-11/SimpleCalculator.java
@@ -0,0 +1,18 @@
+public class SimpleCalculator {
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 5;
+
+		int sum = a + b;
+		int minus = a - b;
+		int times = a * b;
+		double divided = a / b;
+		int rest = a % b;
+
+		System.out.println(sum);
+		System.out.println(minus);
+		System.out.println(times);
+		System.out.println(divided);
+		System.out.println(rest);
+	}
+}
