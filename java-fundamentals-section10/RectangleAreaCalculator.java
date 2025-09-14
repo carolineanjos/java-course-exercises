5cc681dfc7763187d69813f1cf853f3b3f9b8502
carolineanjos <carolineanjoss02@gmail.com>
2025-09-14 17:40:20 -0300

    chore: rename folder name

---
+++ java-fundamentals-section10/RectangleAreaCalculator.java
@@ -0,0 +1,10 @@
+public class RectangleAreaCalculator {
+	public static void main(String[] args) {
+		double height = 8.0;
+		double width = 4.5;
+		double area = width * height;
+
+		System.out.println(area);
+
+	}
+}
