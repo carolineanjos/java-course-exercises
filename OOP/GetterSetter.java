8f2d11f554f75dc612df4ac3a05d0ddefa5867ab
carolineanjos <carolineanjoss02@gmail.com>
2025-09-16 17:36:25 -0300

    chore: implements getter and sette's concepts

---
+++ OOP/GetterSetter.java
@@ -0,0 +1,11 @@
+class GetterSetter {
+    private String nome;
+     
+    public void setName(String novoNome) {  // Setter para definir o nome
+        nome = novoNome;
+    }
+     
+    public String getName() {  // Getter para obter o nome
+        return nome;
+    }
+}
---
+++ OOP/SchoolOffice.java
@@ -0,0 +1,10 @@
+class SchoolOffice {
+    public static void main(String[] args) {
+
+        GetterSetter student = new GetterSetter();
+
+        student.setName("Maria");
+
+        System.out.println("Nome do aluno: " + student.getName());
+    }
+}
